c337d696724ea9c6168d58afb16a74af63b56740
Aljosha Armitano <aljosha@antmine.io>
2017-02-22 18:41:49 -0500

    remove deprecated warning when using query params

--- backend/src/controllers/mention-controller.js
+++ backend/src/controllers/mention-controller.js
@@ -4,11 +4,11 @@ const orm = require('../common/orm');
 module.exports = function(app) {
     // Create a new mention
     app.post('/users/:userId/pictures/:pictureId/mention', (req, res) => {
-      service.creationMention(req.param('userId'), req.param('pictureId'), req.body, req.user, res);
+      service.creationMention(req.params.userId, req.params.pictureId, req.body, req.user, res);
     });
 
     // Delete a mention
     app.delete('/users/:userId/pictures/:pictureId/mention', (req, res) => {
-      service.deleteMention(req.param('userId'), req.param('pictureId'), req.body, req.user, res);
+      service.deleteMention(req.params.userId, req.params.pictureId, req.body, req.user, res);
     });
 }
