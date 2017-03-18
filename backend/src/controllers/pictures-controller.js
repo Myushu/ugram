dcf82ae6b16aeaa4d6e3eed524bee736e1258e7e
Aljosha Armitano <aljosha@antmine.io>
2017-03-18 11:49:39 -0400

    remove some useless orm var on controllers and add create /search/users, /search/description and /search/hashatg route

--- backend/src/controllers/pictures-controller.js
+++ backend/src/controllers/pictures-controller.js
@@ -1,5 +1,4 @@
 const service = require('../services/picture-service');
-const orm = require('../common/orm');
 
 module.exports = function(app) {
 
@@ -12,9 +11,4 @@ module.exports = function(app) {
     app.get('/picture', (req, res) => {
       service.getPicture(req.query.filename, res);
     });
-
-    // Search picture
-    app.get('/pictures/search', (req, res) => {
-      service.searchPicture(req.query, res);
-    })
 }
---
+++ backend/src/services/search-service.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const config = require('config');
+const logger = require('../common/logger');
+const orm = require('../common/orm');
+const queryManager = require('../common/queryManager');
+const userModel = orm.getSequelize().import("../models/USER.js");
+const pictureModel = orm.getSequelize().import("../models/PICTURE.js");
+const hashtagModel =  orm.getSequelize().import("../models/HASHTAG.js");
+
+pictureModel.hasMany(hashtagModel, {foreignKey : 'ID_PICTURE'});
+pictureModel.belongsTo(userModel, {foreignKey : 'ID_OWNER'});
+
+exports.searchUser = (query, res) => {
+  var attributes = {
+    where : {
+      $or : [
+        {
+          'FIRSTNAME' : {
+            $like : '%' + query.input + '%'
+          },
+        }, {
+          'LASTNAME' : {
+            $like : '%' + query.input + '%'
+          },
+        }, {
+          'PSEUDO' : {
+            $like : '%' + query.input + '%'
+          }
+        }
+      ]
+    }
+  }
+  var attributesCount = Object.assign({}, attributes);;
+  queryManager.fillAttributesFromQuery(attributes, query);
+  orm.findAllAndCount(userModel, res, attributes, attributesCount);
+}
+
+exports.searchDescription = (query, res) => {
+  var attributes = {
+    where : {
+      'DESCRIPTION' : {
+        $like : '%' + query.input + '%'
+      }
+    }
+  }
+  var attributesCount = Object.assign({}, attributes);;
+  queryManager.fillAttributesFromQuery(attributes, query);
+  orm.findAllAndCount(pictureModel, res, attributes, attributesCount);
+}
+
+exports.searchHashtag = (query, res) => {
+  var attributes = {
+    include : [{
+      model : hashtagModel,
+      // attributes
+      where : {
+        'HASHTAG' : {
+          $like : '%' + query.input + '%'
+        }
+      }
+    }]
+  }
+  var attributesCount = Object.assign({}, attributes);;
+  queryManager.fillAttributesFromQuery(attributes, query);
+  orm.findAllAndCount(pictureModel, res, attributes, attributesCount);
+}
