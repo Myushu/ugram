4aca24566f3dfd1ae39129dbd7bfbacdbe19c57e
bousca <antonin.bouscarel@epitech.eu>
2017-03-25 13:50:43 -0400

    fix issur

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -37,44 +37,6 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.picture_url = this.configService.getUrl();
-
-    /*this.socket = io.connect(this.configService.getWSUrl());
-    this.socket.on("connect", function() {
-      this.socket.emit("authenticate", {token: this._cookieService.get('token')})
-        .on('authenticated', function() {
-          console.log('okkkkk');
-        })
-        .on('unauthorized', function(message) {
-          console.log('error', message);
-        });
-    });*/
-
-
-
-
-
-
-    /*socket.on('connect', function () {
-      socket
-        .emit('authenticate', {token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFudG9uaW5AZ21haWwuY29tIiwidXNlcklkIjoxNCwiaWF0IjoxNDkwMTIxMzA5LCJleHAiOjE0OTAyMDc3MDl9.p3hzQhYyKDMtB6POwNJbqMm_7bqQqusaz1kz7NECVXo"}) //send the jwt
-        .on('authenticated', function () {
-          socket.emit('message', {name: 'test'}, function(data) {
-            console.log('dataaa', data);
-          });
-        })
-        .on('unauthorized', function(msg) {
-          console.log("unauthorized: " + JSON.stringify(msg.data));
-          throw new Error(msg.data.type);
-        });
-    });*/
-
-    /*this.socket.on("messages", (data) => console.log('plop', data));
-    this.socket.emit('connection', 'tobi', function (data) {
-      console.log('ws', data); // data will be 'woot'
-    });
-    this.socket.emit('join', JSON.stringify({token: this._cookieService.get('token')}), function(data) {
-      console.log('data', data);
-    });*/
   }
 
   onPager(event: number): void {
