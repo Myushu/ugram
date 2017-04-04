cb90c198b3930c21d7ffde580ae68867c4c2a2e9
Aljosha Armitano <aljosha@antmine.io>
2017-04-04 15:26:23 -0400

    add user status connected/disconnect and notify users when status is updated

--- backend/src/services/socket-service.js
+++ backend/src/services/socket-service.js
@@ -3,17 +3,21 @@ const logger = require("../common/logger");
 const jwtDecode = require('jwt-decode');
 const redisToken = require('../common/redisToken')
 const socketManager = require("../common/socketManager");
+const userService = require('../services/users-service');
+const notificationManager = require('../common/notificationManager');
 
 const messageModel = orm.getSequelize().import("../models/MESSAGE.js");
-;
+
 exports.join = (data, client) => {
   try {
     var user = jwtDecode(data);
     redisToken.isTokenOnRedis(data, user, function (err, repplies) {
-      if (repplies == user.userId)
+      if (repplies == user.userId) {
         socketManager.addClient(user.userId, user.pseudo, client);
-      else
-        client.emit('errors', 'Invalid token');
+        userService.setConnectStatus(true,  user.userId);
+        notificationManager.notifyConnection(client, user.userId);
+      } else
+        client.disconnect('unauthorized');
     });
   } catch (e) {
     logger.error(e.message);
@@ -21,7 +25,11 @@ exports.join = (data, client) => {
 }
 
 exports.disconnect = (client) => {
+  idUser = socketManager.getClientId(client).USER_ID;
+  userService.setConnectStatus(false, idUser);
   socketManager.removeClient(client);
+  notificationManager.notifyDisconnection(client, idUser);
+  client.disconnect('connection closed');
 }
 
 exports.message = (io, client, message) => {
