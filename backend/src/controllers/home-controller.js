27aeb9f1b845d4b2bfbb4bdd48e9faafbe9c6a73
Aljosha Armitano <aljosha@antmine.io>
2017-04-09 15:30:29 -0400

    fix redirection on / url

--- backend/src/controllers/home-controller.js
+++ backend/src/controllers/home-controller.js
@@ -1,9 +1,11 @@
-const service = require('../services/home-service');
 const config = require('../common/configManager');
 
 module.exports = function(app) {
-    // Get the default page
-    app.get('/', (req, res) => {
-      res.redirect(config.get('CLIENT_URL', 'client.url'));
-    });
+  // Get the default page
+  app.get('/', (req, res) => {
+    res.writeHead(301,
+      { Location: config.get('CLIENT_URL', 'client.url') }
+    );
+    res.send();
+  });
 }
